c0ef52aca55dde76d3fe08136323b5f537f8664e
chenxiangyu3344 <chenxiangyu3344@gmail.com>
2018-10-27 21:30:18 +0800

    the first commit.

---
+++ LeetCode/src/Easy/easy94w.java
@@ -0,0 +1,37 @@
+package Easy;
+
+import java.util.Scanner;
+
+import javax.sound.midi.Synthesizer;
+
+/*
+ * 给定一个二叉树，返回它的中序 遍历。
+
+示例:
+
+输入: [1,null,2,3]
+   1
+    \
+     2
+    /
+   3
+
+输出: [1,3,2]
+ */
+public class easy94w {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+        // 从键盘接收数据
+ 
+        // next方式接收字符串
+        System.out.println("next方式接收：");
+        // 判断是否还有输入
+        if (scan.hasNext()) {
+            String str1 = scan.next();
+            System.out.println("输入的数据为：" + str1);
+        }
+        scan.close();
+	}
+
+}
